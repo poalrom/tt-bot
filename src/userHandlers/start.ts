1a361f680a1e58dd4816264315265eaf431fddd3
Alex Popkov <poalrom@yandex.ru>
2019-08-24 14:50:45 +0300

    Add active debate message on start

--- src/userHandlers/start.ts
+++ src/userHandlers/start.ts
@@ -1,4 +1,6 @@
+import { Debate } from "../db/entities/Debate";
 import { User } from "../db/entities/User";
+import { DebateKeyboard } from "../keyboards/DebateKeyboard";
 import { UserKeyboard } from "../keyboards/UserKeyboard";
 import { logger } from "../logger";
 import { Texts } from "../texts";
@@ -10,4 +12,25 @@ export async function start(user: User) {
     await user.resetState();
 
     await userBot.sendMessage(user.chatId, Texts.start_command_response, UserKeyboard(user));
+
+    const activeDebate = await Debate.findOne({ isActive: true });
+
+    if (activeDebate) {
+        await userBot.sendMessage(
+            user.chatId,
+            Texts.has_active_debate
+        );
+        await userBot.sendMessage(
+            user.chatId,
+            Texts.debate_start_message
+                .replace("{1}", String(activeDebate.id))
+                .replace("{2}", activeDebate.theme)
+                .replace("{3}", activeDebate.defender)
+                .replace("{4}", activeDebate.opponent),
+            {
+                ...DebateKeyboard(activeDebate),
+                parse_mode: "Markdown",
+            },
+        );
+    }
 }
